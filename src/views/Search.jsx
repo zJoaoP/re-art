9393855e4d9656db731e5f9d1bc7cb15c0b6d919
JoaoP <contato.joaocomp@gmail.com>
2020-11-22 21:52:32 -0300

    Adding spinner for loading feedback

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import ContentGrid from '../components/ContentGrid/ContentGrid';
 import Header from '../components/Header/Header';
 import api from '../service/rijks';
 
+function Loading() {
+  return (
+    <CircularProgress
+      style={{ display: 'block', top: '50%', right: '50%', position: 'fixed' }}
+    />
+  );
+}
+
 // TODO: Handle empty response from museum.
 export default function Search() {
-  const [content, setContent] = React.useState([]);
   const { searchTerm } = useSelector((state) => state.search);
+  const [content, setContent] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setLoading(true);
     api
       .fetchSearchTerm(searchTerm)
       .then((response) => response.json())
-      .then((response) => setContent(response.artObjects));
+      .then((response) => {
+        setContent(response.artObjects);
+        setLoading(false);
+      });
     return () => {};
   }, [searchTerm]);
 
   return (
     <div id="search-wrapper">
       <Header />
-      <ContentGrid content={content} />
+      {loading ? <Loading /> : <ContentGrid content={content} />}
     </div>
   );
 }
