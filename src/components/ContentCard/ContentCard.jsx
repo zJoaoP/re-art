b215931c98c1bfd46b9163f0ed4789eaa3fb31a8
JoaoP <contato.joaocomp@gmail.com>
2020-11-23 11:50:36 -0300

    Linking to artwork page

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
 import Card from '@material-ui/core/Card';
@@ -13,6 +14,8 @@ import IconButton from '@material-ui/core/IconButton';
 
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
+
+import NoImage from '../../assets/images/no_image.jpg';
 
 import useStyles from './style';
 
@@ -27,14 +30,14 @@ export default function ContentCard({ artwork }) {
           subheaderTypographyProps={{ color: 'secondary' }}
         />
         <CardActionArea>
-          {artwork.hasImage ? (
+          <Link to={`/artwork/${artwork.id}`}>
             <CardMedia
               className={classes.media}
-              image={artwork.webImage.url}
+              image={artwork.hasImage ? artwork.webImage.url : NoImage}
               title={artwork.title}
               component="img"
             />
-          ) : null}
+          </Link>
         </CardActionArea>
         <CardActions>
           <IconButton>
