7a84e9a05172a2dfff7f393f933efd4a6ccf8614
JoaoP <contato.joaocomp@gmail.com>
2020-11-25 12:06:32 -0300

    Changing count of results per page

--- src/service/rijks.js
+++ src/service/rijks.js
@@ -2,7 +2,7 @@ import { RIJKS_API_URL, RIJKS_API_KEY } from '../helpers/config';
 
 const apiSettings = {
   fetchSearchTerm: async (term, page, resultsPerPage = 8) => {
-    const endpoint = `${RIJKS_API_URL}?key=${RIJKS_API_KEY}&q=${term}&p=${page}&ps=${resultsPerPage}`;
+    const endpoint = `${RIJKS_API_URL}?key=${RIJKS_API_KEY}&q=${term}&p=${page}&ps=${resultsPerPage}&imgonly=True`;
     return fetch(endpoint).then((response) => response.json());
   },
   fetchArtworkByNumber: async (number) => {
